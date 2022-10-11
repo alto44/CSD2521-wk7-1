7c3314d785962bc4290fb11c3ddca6664bd10513
Aillsj7952 <111881658+Aillsj7952@users.noreply.github.com>
2022-10-11 10:45:47 -0400

    Update HelloTest1.java
    I added an update on line 3 and modified the comments on line 9.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on October 11, 2022 JA
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request.
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
